60ee2da65a9172c5c4af8808290b1de918dd4178
root <root@instance-1.asia-southeast1-b.c.utopian-caldron-257412.internal>
2019-10-29 13:57:44 +0000

    add init bash file

---
+++ init_your_linux.sh
@@ -0,0 +1,13 @@
+apt update -y &&
+apt upgrade -y &&
+apt autoremove -y &&
+apt install zsh &&
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
+wget https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-x64.tar.xz &&
+tar -xf ./node-v12.13.0-linux-x64.tar.xz &&
+mv ./node-v12.13.0-linux-x64 /usr/local/ &&
+wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz &&
+tar -xf ./go1.13.3.linux-amd64.tar.gz &&
+mv ./go /usr/local/

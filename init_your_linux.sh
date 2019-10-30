7aa1fa7d3ac4d83d2a0beeb49140dab3be573990
pengliheng <pengliheng@gmail.com>
2019-10-30 01:42:07 +0000

    add to create .gitconfig file

--- init_your_linux.sh
+++ init_your_linux.sh
@@ -2,6 +2,8 @@ apt update -y &&
 apt upgrade -y &&
 apt autoremove -y &&
 apt install zsh &&
+touch ~/.gitconfig &&
+echo '[user]\n	name = pengliheng\n	email = pengliheng@gmail.com' >> ~/.gitconfig &&
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&

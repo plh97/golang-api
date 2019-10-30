ca3fd9a3c4f6cf33810449fe2fc6e1ca1e4b3999
pengliheng <pengliheng@gmail.com>
2019-10-30 12:10:45 +0000

    isolate zsh with main sh

--- init_your_linux.sh
+++ init_your_linux.sh
@@ -1,21 +1,19 @@
 apt update -y &&
 apt upgrade -y &&
 apt autoremove -y &&
-apt install zsh -y &&
 apt install gitless -y &&
 touch ~/.gitconfig &&
 echo '[user]' >> ~/.gitconfig &&
 echo '	name = pengliheng' >> ~/.gitconfig &&
 echo '	email = pengliheng@gmail.com' >> ~/.gitconfig &&
-sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
 wget https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-x64.tar.xz &&
 tar -xf ./node-v12.13.0-linux-x64.tar.xz &&
 mv ./node-v12.13.0-linux-x64 /usr/local/ &&
 wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz &&
 tar -xf ./go1.13.3.linux-amd64.tar.gz &&
 mv ./go /usr/local/ &&
+rm ./node-v12.13.0-linux-x64.tar.xz &&
+rm ./go1.13.3.linux-amd64.tar.gz &&
 cp ./id_rsa_github* /etc/ssh/ &&
 vim /etc/ssh/sshd_config &&
 service sshd restart &&
---
+++ init_zsh.sh
@@ -0,0 +1,4 @@
+apt install zsh -y &&
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y &&
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

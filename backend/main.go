cf85e55fd797e600031c8e07b494d011d36d2f67
pengliheng <pengliheng@gmail.com>
2019-11-02 07:36:06 +0000

    feature: store code in github cloud

--- backend/main.go
+++ backend/main.go
@@ -2,38 +2,39 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
-
-	"github.com/gorilla/mux"
+	"time"
 )
 
-// Book is a book struct
-type Book struct {
-	id     			string
-	name  			string
-	author 			string
-	createTime 	int
-	updateTime 	int
+// Todo is a todo list struct
+type Todo struct {
+	id        string    `json:id`
+	name      string    `json:"name"`
+	completed bool      `json:"completed"`
+	due       time.Time `json:"due"`
 }
 
-var books []Book
+var todos []Todo
 
 func main() {
-	r := mux.NewRouter()
-	books = append(books, Book{id: "1", createTime: 2016, updateTime: 2017 , author: "Bob", name: "哈里波塔"})
-	books = append(books, Book{id: "2", createTime: 2016, updateTime: 2017 , author: "Bob", name: "死侍"})
-	books = append(books, Book{id: "3", createTime: 2016, updateTime: 2017 , author: "Bob", name: "超市夜未眠"})
-	books = append(books, Book{id: "4", createTime: 2016, updateTime: 2017 , author: "Bob", name: "吉泽明步"})
-	r.HandleFunc("/api/book", getBooks).Methods("GET")
-	r.HandleFunc("/api/book/create", createBook).Methods("GET")
-	r.HandleFunc("/api/book/update/{id}", updateBook).Methods("GET")
-	r.HandleFunc("/api/book/delete/{id}", deleteBook).Methods("GET")
-	r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	router := mux.NewRouter().StrictSlash(true)
+	todos = []Todo{}
+	todos = append(todos, Todo{id: "1", name: "peng"})
+	todos = append(todos, Todo{id: "2", name: "zhang"})
+	todos = append(todos, Todo{id: "3", name: "dai"})
+	todos = append(todos, Todo{id: "4", name: "lin"})
+	router.HandleFunc("/api/book", getBooks).Methods("GET")
+	router.HandleFunc("/api/book/create", createBook).Methods("GET")
+	router.HandleFunc("/api/book/update/{id}", updateBook).Methods("GET")
+	router.HandleFunc("/api/book/delete/{id}", deleteBook).Methods("GET")
+	router.HandleFunc("/api/book/{id}", getBook).Methods("GET")
 	srv := &http.Server{
-		Handler: r,
+		Handler: router,
 		Addr:    "0.0.0.0:8080",
 	}
 
@@ -43,53 +44,54 @@ func main() {
 // 获取所有书
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+	fmt.Println(todos)
+	json.NewEncoder(w).Encode(todos)
 }
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // get params
-	for _, e := range books {
+	for _, e := range todos {
 		if e.id == params["id"] {
 			json.NewEncoder(w).Encode(e)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(Book{})
+	json.NewEncoder(w).Encode(Todo{})
 }
 
 // 创建书
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	json.NewDecoder(r.Body).Decode(book)
-	book.id = strconv.Itoa(rand.Intn(10000))
-	books = append(books, book)
-	json.NewEncoder(w).Encode(books)
+	var todo Todo
+	json.NewDecoder(r.Body).Decode(&todo)
+	todo.id = strconv.Itoa(rand.Intn(10000))
+	todos = append(todos, todo)
+	json.NewEncoder(w).Encode(todos)
 }
 
 // 更新书
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // get params
-	for i, book := range books {
-		if book.id == params["id"] {
-			var book Book
-			json.NewDecoder(r.Body).Decode(book)
-			books[i] = book
+	for i, todo := range todos {
+		if todo.id == params["id"] {
+			var book Todo
+			json.NewDecoder(r.Body).Decode(&book)
+			todos[i] = book
 			// books = append(books[:i], book, books[i+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(todos)
 }
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // get params
-	for i, book := range books {
-		if book.id == params["id"] {
-			books = append(books[:i], books[i+1:]...)
+	for i, todo := range todos {
+		if todo.id == params["id"] {
+			todos = append(todos[:i], todos[i+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(todos)
 }

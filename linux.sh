56e3b21b9a9cc3fdefc567132e666cddbd914b1b
pengliheng <pengliheng111@gmail.com>
2019-11-06 03:00:27 +0000

    feature: improve bash code

--- linux.sh
+++ linux.sh
@@ -4,7 +4,7 @@ apt autoremove -y &&
 touch ~/.gitconfig &&
 echo '[user]' >> ~/.gitconfig &&
 echo '	name = pengliheng' >> ~/.gitconfig &&
-echo '	email = pengliheng@gmail.com' >> ~/.gitconfig &&
+echo '	email = pengliheng111@gmail.com' >> ~/.gitconfig &&
 wget https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-x64.tar.xz &&
 tar -xf ./node-v12.13.0-linux-x64.tar.xz &&
 mv ./node-v12.13.0-linux-x64 /usr/local/node &&

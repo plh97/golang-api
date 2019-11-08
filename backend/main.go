63018417e1dc93107174930b8b0daeffc65b06ba
pengliheng <pengliheng111@gmail.com>
2019-11-08 02:10:54 +0000

    feature: store file change because of the risk

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
-	// "go.mongodb.org/mongo-driver/bson"
+	// "fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -32,10 +33,11 @@ type Book struct {
 
 var books []Book
 var collection *mongo.Collection
+var ctx context.Context
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.RequestURI)
+		log.Println(r.RequestURI, r.Method)
 		next.ServeHTTP(w, r)
 	})
 }
@@ -55,8 +57,8 @@ func corsMiddleware(next http.Handler) http.Handler {
 }
 
 func handleMongodb() {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@mongodb:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -65,25 +67,7 @@ func handleMongodb() {
 		log.Fatal(err)
 	}
 	log.Println("Connected to MongoDB!")
-	collection = client.Database("test").Collection("trainers")
-}
-
-func handleRoute() {
-	// api
-	r := mux.NewRouter()
-	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "哈里波塔"})
-	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "死侍"})
-	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "超市夜未眠"})
-	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "吉泽明步"})
-	collection.Insert(context.TODO(), books)
-	r.HandleFunc("/api/book/{id}", updateBook).Methods(http.MethodPatch, http.MethodOptions)
-	r.HandleFunc("/api/book/{id}", deleteBook).Methods(http.MethodDelete, http.MethodOptions)
-	r.HandleFunc("/api/book", createBook).Methods(http.MethodOptions, http.MethodPost)
-	r.HandleFunc("/api/book", getBooks).Methods(http.MethodGet)
-	r.HandleFunc("/api/book/{id}", getBook).Methods(http.MethodGet)
-	r.Use(corsMiddleware)
-	r.Use(loggingMiddleware)
-	http.ListenAndServe("0.0.0.0:8080", r)
+	collection = client.Database("testing").Collection("numbers")
 }
 
 func main() {
@@ -93,6 +77,21 @@ func main() {
 
 // 获取所有书
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	cur, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+	for cur.Next(ctx) {
+		var result bson.M
+		err := cur.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
 	json.NewEncoder(w).Encode(books)
 }
 func getBook(w http.ResponseWriter, r *http.Request) {
@@ -108,13 +107,14 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 // 创建书
 func createBook(w http.ResponseWriter, r *http.Request) {
-	insertResult, err := collection.InsertOne(context.TODO(), ash)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var book Book
-	json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(1000000))
+	// json.NewDecoder(r.Body).Decode(&book)
+	// book.ID = strconv.Itoa(rand.Intn(1000000))
+	// insertResult, err := collection.InsertOne(context.TODO(), book)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(1111, insertResult)
 	books = append(books, book)
 	json.NewEncoder(w).Encode(books)
 }
@@ -134,6 +134,7 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(books)
 }
+
 func deleteBook(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r) // get params
 	for i, book := range books {
@@ -143,4 +144,25 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode(books)
+}
+
+func handleRoute() {
+	r := mux.NewRouter()
+	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "哈里波塔"})
+	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "死侍"})
+	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "超市夜未眠"})
+	books = append(books, Book{ID: strconv.Itoa(rand.Intn(1000000)), Author: "peng", CreateAt: time.Now(), UpdateAt: time.Now(), Name: "吉泽明步"})
+	var interfaceSlice []interface{} = make([]interface{}, len(books))
+	for i, d := range books {
+		interfaceSlice[i] = d
+	}
+	collection.InsertMany(context.TODO(), interfaceSlice)
+	r.HandleFunc("/api/book/{id}", updateBook).Methods(http.MethodPatch, http.MethodOptions)
+	r.HandleFunc("/api/book/{id}", deleteBook).Methods(http.MethodDelete, http.MethodOptions)
+	r.HandleFunc("/api/book", createBook).Methods(http.MethodOptions, http.MethodPost)
+	r.HandleFunc("/api/book", getBooks).Methods(http.MethodGet)
+	r.HandleFunc("/api/book/{id}", getBook).Methods(http.MethodGet)
+	r.Use(corsMiddleware)
+	r.Use(loggingMiddleware)
+	http.ListenAndServe("0.0.0.0:8080", r)
 }
